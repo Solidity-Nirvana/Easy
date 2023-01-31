599ebe16e80bb0a3d126a961043412ca3586c57e
JQ <johnrquarnstrom@gmail.com>
2023-01-30 17:49:16 -0700

    Update extended episode structure

---
+++ episodes/19_Globals_Reserved/Reserved.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.16;
+
+contract Reserved {
+
+    // ---------------------
+    //    State Variables
+    // ---------------------
+
+    /*
+        SOURCE: https://docs.soliditylang.org/en/v0.8.11/cheatsheet.html#reserved-keywords
+        after, alias, apply, auto, byte, case, copyof, default, define, final, implements, 
+        in, inline, let, macro, match, mutable, null, of, partial, promise, reference, 
+        relocatable, sealed, sizeof, static, supports, switch, typedef, typeof, var
+    */
+
+
+    // -----------------
+    //    Constructor
+    // -----------------
+
+    constructor() {
+
+    }
+
+
+}
