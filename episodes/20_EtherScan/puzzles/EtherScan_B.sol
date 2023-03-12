2239fc4ea9db2d554dc386098bcbb6b28c4ba339
pseudonaut <jquarnstrom@yahoo.com>
2023-03-12 11:37:30 -0600

    Guide + Puzzle C

--- episodes/20_EtherScan/puzzles/EtherScan_B.sol
+++ episodes/20_EtherScan/puzzles/EtherScan_B.sol
@@ -23,8 +23,16 @@ contract HashBrowns {
         return abi.encode(input);
     }
 
+    function encodeSecret2(address input, uint256 a, uint256 b) external pure returns(bytes memory output) {
+        return abi.encode(input, a, b);
+    }
+
     function decodeSecret(bytes memory data) external pure returns(address output) {
         (output) = abi.decode(data, (address));
+    }
+
+    function decodeSecret2(bytes memory data) external pure returns(address output, uint256 a, uint256 b) {
+        (output, a, b) = abi.decode(data, (address, uint256, uint256));
     }
 
 }
@@ -37,7 +45,7 @@ contract Etherscan_B is Puzzle {
 
     address secret = 0xB99DEdbDe082B8Be86f06449f2fC7b9FED044E15;
 
-    // bytes input = '00000000';
+    bytes input;
 
     function setUp() public {
         HB = new HashBrowns();
@@ -48,7 +56,7 @@ contract Etherscan_B is Puzzle {
     }
 
     function test_EtherScan_B() public {
-        // assertEq(HB.decodeSecret(input), HB.encodeSecret(secret));
+        // assertEq(HB.encodeSecret(secret), HB.encodeSecret(secret));
     }
 
 }
---
+++ episodes/20_EtherScan/puzzles/EtherScan_C.sol
@@ -0,0 +1,1 @@
+// TODO: Verify a contract.
