23616c84521e111fbf9b76552c46752f2f7e036b
pseudonaut <jquarnstrom@yahoo.com>
2023-03-18 08:49:34 -0600

    Contract Fundamentals updated

---
+++ episodes/01_Contract_Fundamentals/scenarios/Contract_Fundamentals_1.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import "utils/Scenario.sol";
+
+// TODO: Explain the minimum contract requirements.
+
+contract Vanilla {
+
+}
+
+contract Contract_Fundamental_1 is Scenario {
+
+    Vanilla VAN;
+
+
+    function setUp() public {
+        // Initialize a new Vanilla contract called VAN
+        VAN = new Vanilla();
+    }
+
+
+    function test_Contract_Fundamentals_1A() public {
+        emit Log(address(VAN));
+    }
+
+}
