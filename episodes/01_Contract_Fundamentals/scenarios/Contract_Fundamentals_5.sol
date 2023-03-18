33bcaf062aa4ebdd127d8728d04801ef90c8b3ec
pseudonaut <jquarnstrom@yahoo.com>
2023-03-18 09:56:42 -0600

    Update 5

--- episodes/01_Contract_Fundamentals/scenarios/Contract_Fundamentals_5.sol
+++ episodes/01_Contract_Fundamentals/scenarios/Contract_Fundamentals_5.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.17;
 
+// NOTE: You can find this contract here, or in the directory /utils/Template.sol
+
 contract Template {
 
     // ---------------------
