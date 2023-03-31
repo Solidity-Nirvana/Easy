1fb2442c3e24b00802f53176ebe1aa90d071e83d
pseudonaut <jquarnstrom@yahoo.com>
2023-03-31 10:35:48 -0400

    Update Comments

--- episodes/01_Contract_Fundamentals/scenarios/Contract_Fundamentals_4.sol
+++ episodes/01_Contract_Fundamentals/scenarios/Contract_Fundamentals_4.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// This is one form of a comment, with two forward slashes
-// These types of comments are single line
+// This comment uses two forward slashes, this is a single-line comment
 
 contract Vanilla {  // You can place single-line comments to the right of code, on the same line
 
     /*
-        This is another form of a comment, with a forward slash and an asterisk
-        to mark the beginning of the comment, and an asterisk and forward slash
-        to mark the end of the comment. This type of comment is multi-line.
+        This is another form of a comment, with a forward slash and an asterisk to mark the beginning, 
+        and an asterisk and forward slash to mark the end. This type of comment is multi-line.
+        You cannot incorporate and compile any code written with multi-line comments.
     */
 
 }
