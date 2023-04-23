dd9dadc3e7f15e83f44c02f6eafad0befe4fdd40
pseudonaut <jquarnstrom@yahoo.com>
2023-04-22 17:21:58 -0700

    Rearrange Scenario Tests

--- episodes/02_Primitive_Types/scenarios/Primitive_Types_3.sol
+++ episodes/02_Primitive_Types/scenarios/Primitive_Types_3.sol
@@ -17,13 +17,13 @@ contract Home {
     address public longWay = address(0x0000000000000000000000000000000000042A);
     address public shortWay = address(0x42A);
 
+    // address type's can be assigned with address(this), in future episode we discuss msg.sender
+    address public something = address(this);
+
     // address values have the following members:
     // address.balance
     // address.code
     // address.codehash
-
-    // address type's can be assigned with address(this), in future episode we discuss msg.sender
-    address public something = address(this);
 
 }
 
@@ -48,23 +48,23 @@ contract Primitive_Types_3 is Scenario {
     }
 
     function test_Primitive_Types_3C() public {
+        emit Log("address(HOME)",   address(HOME));
+        emit Log("HOME.something()", HOME.something());
+        assertEq(address(HOME),      HOME.something());
+    }
+
+    function test_Primitive_Types_3D() public {
         // All contracts inheriting Scenario / Puzzles receive large amount of Ether
         // For more in-depth information, see EVM Fundamentals & Payable Modifier episodes
         emit Log("address(this).balance", address(this).balance);
     }
 
-    function test_Primitive_Types_3D() public {
+    function test_Primitive_Types_3E() public {
         // For more in-depth information, see EVM Fundamentals & Payable Modifier episodes
         // Use .code to get the EVM bytecode as a bytes memory, which might be empty. 
         // Use .codehash to get the Keccak-256 hash of that code (as a bytes32).
         emit Log("address(HOME).code", address(HOME).code);
         emit Log("address(HOME).codehash", address(HOME).codehash);
-    }
-
-    function test_Primitive_Types_3E() public {
-        emit Log("address(HOME)",   address(HOME));
-        emit Log("HOME.something()", HOME.something());
-        assertEq(address(HOME),      HOME.something());
     }
 
 }
