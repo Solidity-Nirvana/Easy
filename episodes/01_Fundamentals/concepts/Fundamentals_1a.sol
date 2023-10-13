e4c2b22dde7e0188624554b0e0fece453cdd1485
pseudonaut <jquarnstrom@yahoo.com>
2023-10-13 16:23:22 -0500

    Save

--- episodes/01_Fundamentals/concepts/Fundamentals_1a.sol
+++ episodes/01_Fundamentals/concepts/Fundamentals_1a.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.17;
 
-contract Vanilla { }
+import "utils/Concept.sol";
+
+// TODO Explain the minimum contract requirements
+
+contract Vanilla {
+
+}
+
+contract Fundamentals_1 is Concept {
+
+    Vanilla VAN;
+
+    function setUp() public {
+        VAN = new Vanilla();
+    }
+
+    // Log a deployed contract address
+
+    function test_Fundamentals_1A() public {
+        emit Log('address(VAN)', address(VAN));
+    }
+
+}
