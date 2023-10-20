327449ddf18c64526a415ef57224001955f2a019
pseudonaut <jquarnstrom@yahoo.com>
2023-10-19 21:51:53 -0500

    Agendas

--- episodes/04_NatSpec/concepts/NatSpec_1.sol
+++ episodes/04_NatSpec/concepts/NatSpec_1.sol
@@ -3,23 +3,19 @@ pragma solidity ^0.8.17;
 
 // TODO: Explain NatSpec
 
-/**
-    @title (Describe the contract) This contract showcases basic NatSpec
-    @notice (Explain the contract) This contract focuses on NatSpec for state variables and the constructor()
-    @author (Name of the author) SolidityNirvana
-
-    The above NatSpec is not required, although more information is always useful
-*/
+/// @title (Describe the contract) This contract showcases basic NatSpec
+/// @notice (Explain the contract) This contract focuses on NatSpec for state variables and the constructor()
+/// @author (Name of the author) SolidityNirvana
 contract NatSpec {
 
     // ---------------------
     //    State Variables
     // ---------------------
 
-    /// @notice Tracks the number of times a function has been called.
+    /// @notice Tracks the number of times a function has been called
     uint256 counter;
 
-    /// The owner of this contract.
+    /// The owner of this contract (this line defaults to an @notice tag when not specified)
     address owner;
 
 
@@ -28,10 +24,19 @@ contract NatSpec {
     //    Constructor
     // -----------------
 
-    /// @notice The constructor() for the NatSpec contract.
-    /// @param _owner Specifies the owner of this contract.
+    // NatSpec is also recognized with comments using the formatting /** */
+
+    /** 
+        @notice The constructor() for the NatSpec contract
+        @param _owner Specifies the owner of this contract
+    */
     constructor(address _owner) {
         owner = _owner;
     }
+
+
+
+    // You can also use NatSpec for events, modifiers, and functions. These will be shown and explained
+    // later when these concepts are first discussed in their respective episodes.
 
 }
