dddcc734a7e8d4a4276a753d6dfe5be82ffd9351
pseudonaut <jquarnstrom@yahoo.com>
2023-10-21 09:24:26 -0500

    Clean Episode 1

--- episodes/01_Contract/concepts/Contract_5.sol
+++ episodes/01_Contract/concepts/Contract_5.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.17;
 
-// TODO Explain the purpose of this "Template" contract
+// TODO Explain the purpose of a "Template" contract
 
-// This contract is also under /utils/Template.sol
-// Do not include sections in a contract that are not present (e.g. Events or Modifiers)
-// This technique will massively increase your ability to navigate a contract as it expands (500+ LOC)
+// This will increase organization, and improve readabiliy.
+// Remove sections not present (for example, do not include "Events" banner if event logs are not present)
+// Also avaialble in /utils/Template.sol
 
 contract Template {
 
