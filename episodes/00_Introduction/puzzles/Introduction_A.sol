0b0f0fd04e9dea8a70dbf8f225aaeebe04e76f91
pseudonaut <jquarnstrom@yahoo.com>
2024-02-14 12:48:45 -0700

    Introduction Comment Cleanup

--- episodes/00_Introduction/puzzles/Introduction_A.sol
+++ episodes/00_Introduction/puzzles/Introduction_A.sol
@@ -18,48 +18,32 @@ contract Simple {
 
 }
 
-// Puzzles are ordered by letters
-// Puzzles are intended to be modified
-// Puzzles have solutions at www.soliditynirvana.com/episodes (must be enrolled)
-// Puzzles will have NOTE tags to explain the objective of the puzzle, which parts should be modified, etc.
-
-// NOTE: Explain to the user (somewhere) that they should work through episodes in sequence
-
 contract Introduction_A is Puzzle {
 
     Simple SIM;
-
-    // This setUp() function runs before every other test function below
 
     function setUp() public {
         SIM = new Simple();
     }
 
-    // Each "test" function runs independently
-
     // Validate state variable "a" equals 1
 
     function test_P_Introduction_A1() public {
 
-        // Puzzles will log outputs to your console with "emit Log()" using the verbosity flag: --verbosity -vvv
-
-        // ├─ emit Log(: SIM.a(), : 0)
         emit Log("SIM.a()", SIM.a());
-
-        // ├─ emit log(: Error: a == b not satisfied [uint])
-        // ├─ emit log_named_uint(key:   Expected, val: 1)
-        // ├─ emit log_named_uint(key:     Actual, val: 0)
+        
         assertEq(SIM.a(), 1);
 
-        // Puzzles will check conditions with "assert()" or "assertEq()", or other advanced means
-        // assertEq() - "Expected" value is the 2nd parameter, and the "Actual" value is the 1st parameter
     }
 
     // Validate state variable "b" equals 2
 
     function test_P_Introduction_A2() public {
+        
         emit Log("SIM.b()", SIM.b());
+
         assertEq(SIM.b(), 2);
+
     }
 
 }
--- episodes/00_Introduction/scenarios/Introduction_1.sol
+++ episodes/00_Introduction/scenarios/Introduction_1.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.17;
 
 import "utils/Scenario.sol";
 
-// TODO Explain the basic components of a scenario
-// Scenarios have TODO tags (at the top) which describe the objective of the Scenario
-
-// Scenarios have one or more contracts (or other elements, i.e libraries and interfaces) defined above
+// TODO Explain what a scenario is
 
 contract Example {
 
@@ -22,63 +19,36 @@ contract Example {
 
 }
 
-// Scenarios are ordered by numbers
-// Scenarios are not intended to be modified
-// Scenarios have explantory videos at www.soliditynirvana.com/episodes (must be enrolled)
-
 contract Introduction_1 is Scenario {
 
-    // Variables are defined here (by type, and then by variable name)
-    // "Example" is the type of variable we are creating, this case in reference to the Example contract
-    // "EX" is the name of the variable
+    // Variables for the scenario
 
     Example EX;
 
-    // This setUp() function runs before every other test function below.
+    // This function sets up the scenario, runs before all test-cases
 
     function setUp() public {
-        
-        // This is the command to deploy a new contract, "new" followed by the contract
-        // The variable "EX" is not initialized until this, we are assigning it to the newly deployed contract
-        EX = new Example();
+        EX = new Example();     // Deploying a new contract for variable "EX"
     }
 
-    // Each of the three functions below will run independently, after setUp() is called, they do not affect each other
-    // In order to run these functions, use the command "forge test --match _Introduction_1" in the CLI
-    // In order to log information in the CLI (command line interface), add "--verbosity -vvv" to the command
-
-    // Showcase a log of the boolean state variable "easy"
+    // Logs a variable, makes an assertion on a boolean
 
     function test_Introduction_1A() public {
         
-        // Emit the log
         emit Log("EX.easy()", EX.easy());
 
-        // Use assert() to validate a boolean input is true
         assert(EX.easy());
     }
 
-    // Showcase a log of the unsigned integer state variable "scenarios"
+    // Logs a variable, makes an equal assertion between two variables
 
     function test_Introduction_1B() public {
         
         emit Log("EX.scenarios()", EX.scenarios());
-
-        // Use assertEq() to compare two primitive types are equal
-        assertEq(EX.scenarios(), 100);
-    }
-
-    // Showcase a log of the unsinged integer state variable "puzzles"
-
-    function test_Introduction_1C() public {
-
         emit Log("EX.puzzles()", EX.puzzles());
 
-        // Use assertEq() to compare, the first is the observed value and the second is the expected value
+        assertEq(EX.scenarios(), 100);
         assertEq(EX.puzzles(), 80);
     }
-
-    // A test function passes if it reaches the end of the function
-    // This means all assertions were succesful, and no functions reverted
 
 }
